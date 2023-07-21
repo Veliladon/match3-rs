742bee368eb76640b12766d2fb764f9deb7b22ce
MB <mburtenshaw@gmail.com>
2023-07-21 10:06:32 -0400

    Added comments to tweening

--- src/tilemove.rs
+++ src/tilemove.rs
@@ -17,16 +17,21 @@ pub fn tile_mover(
     game_board: Res<GameBoard>,
 ) {
     for (entity, mut transform, mut tile_move) in move_query.iter_mut() {
+        // We tick the TileMoving timer along
         tile_move.duration.tick(time.delta());
-        if tile_move.duration.just_finished() {
-            transform.translation.x = tile_move.destination.x as f32 * TILE_WIDTH + HALF_TILE_WIDTH;
-            transform.translation.y =
-                tile_move.destination.y as f32 * TILE_HEIGHT + HALF_TILE_HEIGHT;
 
+        // We need the detination in board space whether the timer is still going or finished
+        let dest_transform = game_board.find_local_from_grid(tile_move.destination);
+
+        // Is the transition finished?
+        if tile_move.duration.just_finished() {
+            // If the transition is finished we clean up the x and y to precisely match and remove the component
+            transform.translation.x = dest_transform.x;
+            transform.translation.y = dest_transform.y;
             commands.entity(entity).remove::<TileMoving>();
         } else {
+            // Otherwise we update the tile's transform based on an easing function
             let origin_transform = game_board.find_local_from_grid(tile_move.origin);
-            let dest_transform = game_board.find_local_from_grid(tile_move.destination);
             let mut final_transform = dest_transform - origin_transform;
             let percent_complete = tile_move.duration.percent();
             let eased_percent = expo_in_out(percent_complete);
